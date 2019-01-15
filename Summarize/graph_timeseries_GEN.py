e6555205c27c3ec733961647c07d47f0426cf349
JoseHernandez9094 <josexavi06@gmail.com>
2019-01-14 20:04:46 -0500

    Data push!
    All data collecting, polishing, and visualization pushed!

---
+++ Summarize/graph_timeseries_GEN.py
@@ -0,0 +1,36 @@
+#This python script will create graphs for each problem based on selection scheme
+#python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+DATA_DIR = '../Data/Polished/Generations/'
+PROBLEMS = ['for-loop-index', 'median', 'small-or-large', 'smallest']
+DIMS = ['CN_1-CS_512', 'CN_2-CS_256', 'CN_4-CS_128', 'CN_8-CS_64', 'CN_16-CS_32', 'CN_32-CS_16', 'CN_64-CS_8' ,'CN_128-CS_4', 'CN_256-CS_2']
+DIM_DICT = {'CN_1-CS_512': 'CN:1-CS:512', 'CN_2-CS_256':'CN:2-CS:256', 'CN_4-CS_128':'CN:4-CS:128' \
+            , 'CN_8-CS_64': 'CN:8-CS:64', 'CN_16-CS_32':'CN:16-CS:32', 'CN_32-CS_16':'CN:32-CS:16', \
+            'CN_64-CS_8': 'CN:64-CS:8' ,'CN_128-CS_4' : 'CN:128-CS:4', 'CN_256-CS_2': 'CN:256-CS:2'}
+SELECTION = ['COHORT_LEX', 'DOWN_SAMPLE_TESTS' ,'PROG_ONLY_COHORT_LEX', 'TRUNCATED']
+
+for prob in PROBLEMS:
+    for sele in SELECTION:
+        for dim in DIMS:
+            
+            PATH = DATA_DIR + prob + '__' + sele + '__' + dim + '.csv'
+            file = Path(PATH)
+            
+            if file.is_file():
+                data = pd.read_csv(PATH)
+                gens = list(data['Generations'])
+                cnt = list(data['Solution_Count'])
+                
+                plt.step(gens, cnt, where='post', label=DIM_DICT[dim])
+                plt.xlabel('Generations')
+                plt.ylabel('Solution Count')
+            
+        
+        plt.title(prob+'_'+sele)
+        plt.legend()
+        plt.savefig('../Data/Figs/Generations/' + prob+'__'+sele+'.png')
+        plt.clf()
---
+++ Summarize/solution_timeseries_EVAL.py
@@ -0,0 +1,94 @@
+#python3
+#This script will make csv so that graph_timeseries.py can create plots with them!
+#TODO: Make the functionality for Evaluations
+
+import pandas as p
+
+MAX_EVAL = 512*512*1000
+df = p.read_csv('../Data/Raw/min_programs__eval_262144000.csv')
+treat = {}
+TREATMENT = 'treatment'
+FOUND = 'solution_found'
+UPDATE = 'update_found'
+EVAL = 'evaluation_found'
+POS_UPDATE = 0
+POS_EVAL = 1
+
+for i,row in df.iterrows():
+    #If we do not have the treatment in our data dict
+    if row[TREATMENT] in treat:
+        if row[FOUND] == True:
+            #If the row has found a solution store gen and eval
+            tup = tuple([float(row[UPDATE]), float(row[EVAL])])
+            treat[row[TREATMENT]].append(tup)
+    else:
+        if row[FOUND] == True:         
+            temp = [tuple([float(row[UPDATE]), float(row[EVAL])])]
+            treat[row[TREATMENT]] = temp
+
+#Will gather data by problem into CN,CS lists for generations.
+#TODO
+K_PROB = 0
+K_SEL = 1
+K_CN = 2
+K_CS = 3
+data_gen = {}
+for k,val in treat.items():
+    k = k[8:].split('__')
+    gens = [x[POS_EVAL] for x in val]
+    gens.sort()
+    dimen = k[K_CN] + '-' + k[K_CS]
+    prob = k[K_PROB]
+    sele = k[K_SEL]
+
+    #check if problem exists within the first layer of dict
+    if prob not in data_gen:
+        #If not in the dict, create an empty one for it
+        data_gen[prob] = {}
+
+        #Check if selection not within the second layer
+        if sele not in data_gen[prob]:
+            #Second level is the selection scheme
+            data_gen[prob][sele] = {}
+            #Third level is the dimensionality
+            data_gen[prob][sele][dimen] = gens
+
+        #Selection is within the second layer
+        else:
+            #Third level is the dimensionality
+            data_gen[prob][sele][dimen] = gens
+
+    else:
+        #Check if selection not within the second layer
+        if sele not in data_gen[prob]:
+            #Second level is the selection scheme
+            data_gen[prob][sele] = {}
+            #Third level is the dimensionality
+            data_gen[prob][sele][dimen] = gens
+
+        #Selection is within the second layer
+        else:
+            #Third level is the dimensionality
+            data_gen[prob][sele][dimen] = gens
+
+#Go through each problem
+for prob in data_gen:
+    #Go through each selection scheme
+    for sele in data_gen[prob]:
+        #Go through each dimensionality
+        for dimen in data_gen[prob][sele]:
+            raw = []
+            raw.append(tuple([0,0]))
+            d = data_gen[prob][sele][dimen]
+            #Create the coordinates
+            for i in range(0, len(d)):
+                # raw.append(tuple([d[i], raw[len(raw)-1][1]]))
+                raw.append(tuple([d[i], raw[len(raw)-1][1]+1]))
+            raw.append([MAX_EVAL, raw[len(raw)-1][1]])
+
+            gen = [x[0] for x in raw]
+            cnt = [x[1] for x in raw]
+            raw_data = {'Evaluation': gen, 'Solution_Count': cnt}
+            df = p.DataFrame(raw_data, columns = ['Evaluation', 'Solution_Count'])
+            fname = prob + '__' + sele[4:] + '__' + dimen + '.csv'
+            df.to_csv('../Data/Polished/Evaluations/'+fname)
